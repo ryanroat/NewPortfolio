b5e7c7349419f086d73933660475ae53e4a130a5
Ryan Roat <ryanroat@gmail.com>
2018-02-07 11:29:32 -0500

    used jQuery/AJAX to load content without reloading page

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#menu-content").on("click", "a.item", function(e) {
+    e.preventDefault();
+    console.log("clicked");
+    var link = $(this).attr("id");
+    updateHTMLContent(link);
+    /* console.log(link); */
+  })
+
+  function updateHTMLContent(link) {
+    var loadfile = "./content/" + link + ".html";
+
+    console.log(loadfile);
+
+    $('.content').load(loadfile);
+  }
+})
